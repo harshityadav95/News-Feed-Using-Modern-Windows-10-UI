afc0a98635a6774e02cb7a18264ac64d33ae8425
harshit <harshityadav95@gmail.com>
2016-12-10 23:49:29 +0530

    Updates in interface

--- news_feed_Ui/Form1.Designer.cs
+++ news_feed_Ui/Form1.Designer.cs
@@ -28,27 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(38, 64);
+            this.richTextBox1.Location = new System.Drawing.Point(46, 103);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(205, 202);
+            this.richTextBox1.Size = new System.Drawing.Size(217, 134);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // metroProgressSpinner1
+            // 
+            this.metroProgressSpinner1.Location = new System.Drawing.Point(466, 187);
+            this.metroProgressSpinner1.Maximum = 100;
+            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
+            this.metroProgressSpinner1.Size = new System.Drawing.Size(16, 16);
+            this.metroProgressSpinner1.TabIndex = 1;
+            this.metroProgressSpinner1.UseSelectable = true;
+            this.metroProgressSpinner1.Click += new System.EventHandler(this.metroProgressSpinner1_Click);
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Location = new System.Drawing.Point(440, 142);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.Size = new System.Drawing.Size(133, 163);
+            this.metroTabControl1.TabIndex = 2;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(168, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(286, 44);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(768, 349);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 328);
+            this.BackImage = ((System.Drawing.Image)(resources.GetObject("$this.BackImage")));
+            this.ClientSize = new System.Drawing.Size(1077, 416);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroTabControl1);
+            this.Controls.Add(this.metroProgressSpinner1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "News Feed";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +106,11 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner1;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- news_feed_Ui/Form1.cs
+++ news_feed_Ui/Form1.cs
@@ -22,11 +22,26 @@ namespace news_feed_Ui
             WebClient webClient = new WebClient();
             richTextBox1.Text = webClient.DownloadString("https://raw.githubusercontent.com/harshityadav95/exp_cmd/master/Rss%20feed.txt");
 
+            //webBrowser1.Navigate("https://mobile.twitter.com/Lingayasuniv");
+            webBrowser1.Navigate("http://lukeyscore.blogspot.in/?m=1");
+            //http://go.microsoft.com/fwlink/?linkid=85889&clcid=409
+
+
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void metroProgressSpinner1_Click(object sender, EventArgs e)
+        {
+            metroProgressSpinner1.Speed = 30;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
         }
     }
 }
